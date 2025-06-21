b9a19aaa21518a2c949aeddd22d308b531d683e4
Black leg nameko <sabuchan1112@iclooud.com>
2025-06-22 03:09:27 +0900

    fix hamburger-menu toggle

--- script/main.js
+++ script/main.js
@@ -4,3 +4,13 @@ const mobileMenu = document.getElementById('mobileMenu');
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
 });
+
+
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = mobileMenu.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+
+    if(!isClickInsideMenu && !isClickOnHamburger) {
+        mobileMenu.classList.remove("show");
+    }
+});
