ece456ed8ab3f82dc7e8e5a5f0405b3e717531b4
black-leg-nameko <sabuchan1112@icloud.com>
2026-01-18 10:58:34 +0900

    add vercel deploying files

--- portfolio-spa/vite.config.ts
+++ portfolio-spa/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/',
   build: {
     target: 'es2020',
     cssMinify: true,
